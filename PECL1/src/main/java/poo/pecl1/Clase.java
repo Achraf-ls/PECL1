9de786161591a65be773efc43d45ce2c70cddfd6
giiselaa <gisela.gonzalez@edu.uah.es>
2024-02-05 18:15:17 +0100

    creacion clase

---
+++ PECL1/src/main/java/poo/pecl1/Clase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo.pecl1;
+
+/**
+ *
+ * @author giise
+ */
+public class Clase {
+    
+}
