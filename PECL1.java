2709fc4dfb55fae59a7686ba3e853100238f2509
Achraf-ls <146070220+Achraf-ls@users.noreply.github.com>
2024-02-05 18:03:31 +0100

    Add files via upload

---
+++ PECL1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pecl1;
+
+/**
+ *
+ * @author achra
+ */
+public class PECL1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
