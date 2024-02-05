300805c87c0ed9477978102726dcbddb94f08ac8
giiselaa <gisela.gonzalez@edu.uah.es>
2024-02-05 18:11:43 +0100

    hola

---
+++ PECL1/src/main/java/poo/pecl1/PECL1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.pecl1;
+
+/**
+ *
+ * @author giise
+ */
+public class PECL1 {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA AMIGO");
+    }
+}
